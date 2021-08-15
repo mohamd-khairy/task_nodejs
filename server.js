54e27ba67e3c5b02d92538445dbc7c1516a01eb1
mohamed-khairy <m.khairy@evntoo.com>
2021-08-15 15:15:52 +0200

    fix

--- server.js
+++ server.js
@@ -4,18 +4,18 @@ const app = express();
 
 global.__basedir = __dirname;
 
-var corsOptions = {
-  origin: "http://localhost:5001"
-};
+// var corsOptions = {
+//   origin: "http://localhost:5001"
+// };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 
 const initRoutes = require("./src/routes");
 
 app.use(express.urlencoded({ extended: true }));
 initRoutes(app);
 
-let port = process.env.port || 5000;
+let port = process.env.port || 3000;
 app.listen(port, () => {
   console.log(`Running at localhost:${port}`);
 });
