ee7d7d38e6eecfb0783e3f2aab6dec6326611984
mohamed-khairy <m.khairy@evntoo.com>
2021-08-15 15:33:39 +0200

    fix

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const cors = require("cors");
 const express = require("express");
 const app = express();
 
+let port = process.env.PORT || 5000;
+
 global.__basedir = __dirname;
 
 var corsOptions = {
-  origin: "http://localhost:5001"
+  origin: "https://minlybe.herokuapp.com/"
 };
 
 app.use(cors(corsOptions));
@@ -15,8 +17,5 @@ const initRoutes = require("./src/routes");
 app.use(express.urlencoded({ extended: true }));
 initRoutes(app);
 
-let port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log(`Running at localhost:${port}`);
-});
+app.listen(port);
 
