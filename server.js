00a16a797420e38e1f7062fed3a811b768ff40c8
mohamed-khairy <m.khairy@evntoo.com>
2021-08-16 11:52:24 +0200

    fix

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const cors = require("cors");
 const express = require("express");
 const app = express();
 const path = require('path')
+const fs = require('fs');
 
 let port = process.env.PORT || 5000;
 
@@ -18,6 +19,34 @@ const initRoutes = require("./src/routes");
 
 app.use(express.urlencoded({ extended: true }));
 initRoutes(app);
+
+const router = express.Router();
+
+
+router.get("/files", function (request, response) {
+
+  const directoryPath = __basedir + "/public/"
+
+  fs.readdir(directoryPath, function (err, files) {
+    if (err) {
+      res.status(500).send({
+        message: "Unable to scan files!",
+      });
+    }
+
+    let fileInfos = [];
+
+    files.forEach((file) => {
+      fileInfos.push({
+        name: file,
+        url: path.join(__dirname, '/public', file)
+      });
+    });
+
+    res.status(200).send(fileInfos);
+  });
+
+});
 
 app.listen(port);
 
